ab33e085d2ff62e081dd9d1bd5b274d2fed62cd5
Hecedu <hector.maganahdez@gmail.com>
2021-12-15 20:00:43 -0700

    Updates

---
+++ precache-manifest.2330cb45a2a45441dc53a56566f432b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c9987d5c74e40902017bb090f9e3ab",
+    "url": "/Hecedu/Hector-Portfolio/index.html"
+  },
+  {
+    "revision": "608cb4dc59bc9c62c6a5",
+    "url": "/Hecedu/Hector-Portfolio/static/css/2.4afd467b.chunk.css"
+  },
+  {
+    "revision": "fe0fac92b5684a206cc5",
+    "url": "/Hecedu/Hector-Portfolio/static/css/main.dcb60d16.chunk.css"
+  },
+  {
+    "revision": "608cb4dc59bc9c62c6a5",
+    "url": "/Hecedu/Hector-Portfolio/static/js/2.69665e32.chunk.js"
+  },
+  {
+    "revision": "7811574fa74309e64658af88faa59f50",
+    "url": "/Hecedu/Hector-Portfolio/static/js/2.69665e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe0fac92b5684a206cc5",
+    "url": "/Hecedu/Hector-Portfolio/static/js/main.288f6947.chunk.js"
+  },
+  {
+    "revision": "52b0c581c9a3651d1c36",
+    "url": "/Hecedu/Hector-Portfolio/static/js/runtime-main.9397195f.js"
+  }
+]);
